936a4cb534f740137590552c483d329124c7ce69
wfilho91 <wellington79620@gmail.com>
2021-03-22 19:25:03 -0300

    Correction of the import of the EntityNotFoundException class

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-import javax.persistence.EntityNotFoundException;
+import com.devsuperior.dscatalog.services.exceptions.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
