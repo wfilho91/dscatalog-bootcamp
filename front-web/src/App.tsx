c2d05918b0895fb910369134597fb2eedaf8e00a
wfilho91 <wellington79620@gmail.com>
2021-03-31 15:48:05 -0300

    Add Navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import './core/assets/styles/custom.scss';
+import './App.scss';
+import Navbar from './core/components/Navbar';
 
 const App = ()=>{
    
     return (
-        <div className="container">
-            
-        </div>
+        <Navbar/>
     );
 }
 
