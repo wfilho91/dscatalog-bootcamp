00eaf33080f6fa8f71a8771e294dac0a71e820fb
wfilho91 <wellington79620@gmail.com>
2021-04-14 17:21:01 -0300

    Add product listing with backend

--- front-web/src/pages/Catalog/components/ProductCard/index.tsx
+++ front-web/src/pages/Catalog/components/ProductCard/index.tsx
@@ -1,18 +1,21 @@
-import React from 'react';
-import './styles.scss';
-import {ReactComponent as ProductImage} from '../../../../core/assets/images/product.svg';
-import ProductPrice from '../../../../core/components/ProductPrice';
+import React from "react";
+import "./styles.scss";
+import ProductPrice from "../../../../core/components/ProductPrice";
+import { Product } from "../../../../core/types/Product";
 
-const ProductCard=()=>(
-    <div className="card-base border-radius-10 product-card">
-        <ProductImage/>
-        <div className="product-info">
-           <h6 className="product-name">
-           Computador Desktop - Intel Core i7
-           </h6> 
-           <ProductPrice price ="2.799,00"/>
-        </div>
+type Props = {
+  product: Product;
+};
+const ProductCard = ({ product }: Props) => (
+  <div className="card-base border-radius-10 product-card">
+      <img src={product.imgUrl} alt={product.name} className="product-card-image"/>
+    <div className="product-info">
+      <h6 className="product-name">
+          {product.name}
+      </h6>
+      <ProductPrice price={product.price} />
     </div>
+  </div>
 );
 
 export default ProductCard;
--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,17 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { ProductsResponse } from "../../core/types/Product";
+import { makeRequest } from "../../core/utils/request";
 import ProductCard from "./components/ProductCard";
 import "./styles.scss";
 
-const Catalog = () => (
-  <div className="catalog-container">
-    <h1 className="catalog-title">Catálogo de produtos</h1>
-    <div className="catalog-products">
-      <Link to="/products/:productId">
-        <ProductCard />
-      </Link>
+const Catalog = () => {
+  
+  //Quando a lista de produtos estiver disponivel,
+  // popular um estado, e listar os produtos dinâmicamente
+  const [productsResponse,setProductsResponse]=useState<ProductsResponse>();
+  
+// quando o componente iniciar buscar a lista de produtos
+  useEffect(()=>{
+    const params={
+      page:0,
+      linesPerPage:12
+    }
+
+   makeRequest({url:'/products',params})
+    .then(response=>setProductsResponse(response.data));
+  },[]);
+
+  return (
+    <div className="catalog-container">
+      <h1 className="catalog-title">Catálogo de produtos</h1>
+      <div className="catalog-products">
+        {productsResponse?.content.map(product=>(
+          <Link to={`/products/${product.id}`} key={product.id}>
+          <ProductCard product={product}/>
+        </Link>
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Catalog;
