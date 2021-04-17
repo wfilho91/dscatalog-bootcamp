60d9914c4adcdc7caac474cabadafff6790bc4a2
wfilho91 <wellington79620@gmail.com>
2021-04-17 18:39:14 -0300

    Add pagination

---
+++ front-web/src/core/components/Pagination/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './styles.scss';
+import { ReactComponent as ArrowIcon } from 'core/assets/images/arrow.svg'
+import { generateList } from 'core/utils/list';
+
+type Props={
+    totalPages:number;
+    activePage:number;
+    onChange:(item:number)=>void;
+}
+const Pagination = ({totalPages,activePage,onChange}:Props) => {
+    const items = generateList(totalPages);
+    return (
+        <div className="pagination-container">
+            <ArrowIcon className="pagination-previous" />
+            {items.map(item => (
+                <div
+                    key={item}
+                    className={`pagination-item  ${item === activePage ? 'active':''}`}
+                    onClick={()=>onChange(item)}
+                    >
+                    {item + 1}
+                   
+                </div>
+            ))}
+            <ArrowIcon className="pagination-next" />
+        </div>
+    );
+}
+
+export default Pagination;
--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -5,44 +5,53 @@ import { makeRequest } from "../../core/utils/request";
 import ProductCard from "./components/ProductCard";
 import ProductCardLoader from "./components/Loaders/ProductCardLoader";
 import "./styles.scss";
+import Pagination from "core/components/Pagination";
 
 const Catalog = () => {
-  
   //Quando a lista de produtos estiver disponivel,
   // popular um estado, e listar os produtos dinâmicamente
-  const [productsResponse,setProductsResponse]=useState<ProductsResponse>();
-  const [isLoading,setIsLoading]=useState(false);
-// quando o componente iniciar buscar a lista de produtos
-  useEffect(()=>{
-    const params={
-      page:0,
-      linesPerPage:12
-    }
-    
-  // iniciando o loader
-  setIsLoading(true);
+  const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
+  const [isLoading, setIsLoading] = useState(false);
+  const [activePage, setActivePage] = useState(0);
+  // quando o componente iniciar buscar a lista de produtos
+  useEffect(() => {
+    const params = {
+      page: activePage,
+      linesPerPage: 12,
+    };
 
-   makeRequest({url:'/products',params})
-    .then(response=>setProductsResponse(response.data))
-    .finally(()=>{
-      //finalizar loader
-      setIsLoading(false);
-    });
-  },[]);
+    // iniciando o loader
+    setIsLoading(true);
+
+    makeRequest({ url: "/products", params })
+      .then((response) => setProductsResponse(response.data))
+      .finally(() => {
+        //finalizar loader
+        setIsLoading(false);
+      });
+  }, [activePage]);
 
   return (
     <div className="catalog-container">
       <h1 className="catalog-title">Catálogo de produtos</h1>
       <div className="catalog-products">
-       {isLoading ? <ProductCardLoader/>:(
-          productsResponse?.content.map(product=>(
+        {isLoading ? (
+          <ProductCardLoader />
+        ) : (
+          productsResponse?.content.map((product) => (
             <Link to={`/products/${product.id}`} key={product.id}>
-            <ProductCard product={product}/>
-          </Link>
+              <ProductCard product={product} />
+            </Link>
           ))
-       )}
-      
+        )}
       </div>
+      {productsResponse && (
+        <Pagination
+          totalPages={productsResponse?.totalPages}
+          activePage={activePage}
+          onChange={page=>setActivePage(page)}
+        />
+      )}
     </div>
   );
 };
