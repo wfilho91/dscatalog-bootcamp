65a97b53d6e692f05b0880e7a2408a5fa98988eb
wfilho91 <wellington79620@gmail.com>
2021-04-19 17:02:31 -0300

    Add Admin navbar

--- front-web/src/core/components/Pagination/index.tsx
+++ front-web/src/core/components/Pagination/index.tsx
@@ -10,9 +10,14 @@ type Props={
 }
 const Pagination = ({totalPages,activePage,onChange}:Props) => {
     const items = generateList(totalPages);
+    const previousClass= totalPages > 0 && activePage > 0 ? 'page-active' : 'page-inactive';
+    const nextClass= (activePage + 1) < totalPages ? 'page-active' : 'page-inactive';
     return (
         <div className="pagination-container">
-            <ArrowIcon className="pagination-previous" />
+            <ArrowIcon 
+            className={`pagination-previous ${previousClass}`}
+            onClick={()=>onChange(activePage -1 )}
+            />
             {items.map(item => (
                 <div
                     key={item}
@@ -23,7 +28,10 @@ const Pagination = ({totalPages,activePage,onChange}:Props) => {
                    
                 </div>
             ))}
-            <ArrowIcon className="pagination-next" />
+            <ArrowIcon 
+            className={`pagination-next ${nextClass}`}
+            onClick={()=>onChange(activePage + 1 )}
+            />
         </div>
     );
 }
